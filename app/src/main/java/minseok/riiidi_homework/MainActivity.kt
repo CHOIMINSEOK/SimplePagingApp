b11911e1878fb9d018ab9d97180795f47270e586
Minseok <cms3718@gmail.com>
2020-07-15 15:27:15 +0900

    Init project

---
+++ app/src/main/java/minseok/riiidi_homework/MainActivity.kt
@@ -0,0 +1,11 @@
+package minseok.riiidi_homework
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
